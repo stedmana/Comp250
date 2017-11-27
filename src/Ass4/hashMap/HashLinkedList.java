1a2d11fd3480f8090ba876525485b7352a7dd302
Alex Stedman <Alex@Alexs-MacBook-Air.local>
2017-11-26 20:52:57 -0500

    Finished hashLinkedList, not tested

--- src/Ass4/hashMap/HashLinkedList.java
+++ src/Ass4/hashMap/HashLinkedList.java
@@ -1,7 +1,5 @@
 package Ass4.hashMap;
 
-import HashNode.java;
-
 
 public class HashLinkedList<K,V>{
 	/*
@@ -27,7 +25,9 @@ public class HashLinkedList<K,V>{
 
 	public void add(K key, V value){
 		// ADD CODE BELOW HERE
-
+		HashNode<K,V> temp = new HashNode<K,V>(key, value);
+		temp.next = head;
+		this.head = temp;
 		// ADD CODE ABOVE HERE
 	}
 
@@ -38,7 +38,19 @@ public class HashLinkedList<K,V>{
 
 	public HashNode<K,V> getListNode(K key){
 		// ADD CODE BELOW HERE
-
+		HashNode<K,V> currentNode = this.head;
+		while (currentNode.next != null) {
+			if (currentNode.getKey().equals(key)) {
+				return currentNode;
+			}
+			else {
+				currentNode = currentNode.next;
+			}
+		}
+		if (currentNode.getKey().equals(key)) {
+			return currentNode;
+		}
+		return null;
 		// ADD CODE ABOVE HERE
 	}
 
@@ -50,9 +62,10 @@ public class HashLinkedList<K,V>{
 
 	public HashNode<K,V> removeFirst(){
 		// ADD CODE BELOW HERE
-
+		HashNode<K,V> currentNode = this.head;
+		this.head = currentNode.next;
 		// ADD CODE ABOVE HERE
-		return null; //CODE STUB.. REMOVE THIS LINE
+		return currentNode; //CODE STUB.. REMOVE THIS LINE
 	}
 
 	/*
